c532be4a647ce44d3f36ec2bd931991909d8df04
sskkun <sskkun@gmail.com>
2013-05-28 23:58:13 +0000

    criado metodos xml2projeto e projeto2xml persistindo os projetos e coletando o projeto apartir de um ID especifico. Criado o metodo que retorna o ultimo ID.

---
+++ VisProj/src/main/java/br/ufg/inf/visproj/repositorio/recuperaXML.java
@@ -0,0 +1,24 @@
+package br.ufg.inf.visproj.repositorio;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Package: br.ufg.inf.visproj.repositorio
+ * Class: recuperaXML
+ * User: ArthurNote
+ * Date: 27/05/13
+ * Time: 17:56
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class recuperaXML {
+
+    public static void main(String[] args) {
+
+        PackOut pk = new PackOut();
+
+
+        System.out.println(pk.coletaUltimoIdProjeto());
+
+    }
+
+}
